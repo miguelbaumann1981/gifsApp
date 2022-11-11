46da45dbeb61b04d5bf2b50fb966cb14c682cf53
MiguelMartinezBBVA <miguel.martinez3.contractor@bbva.com>
2022-11-11 17:47:37 +0100

    subida a git

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent {
+
+  constructor( private gifsService: GifsService ) {}
+
+  get historial() {
+    return this.gifsService.historial;
+  }
+
+  imprimir(busqueda: string) {
+    this.gifsService.buscarGifs(busqueda);
+  }
+
+}
